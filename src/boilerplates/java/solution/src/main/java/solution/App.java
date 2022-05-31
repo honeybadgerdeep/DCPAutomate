9f2acbf3d9e4fe1528db8aae129fb63d053bdd6c
honeybadgerdeep <honeybadgerdeep@Deeps-MacBook-Air.local>
2022-05-31 12:38:18 -0700

    Boilerplating file structure complete with language support for Java, Go, Javascript, and Python

---
+++ src/boilerplates/java/solution/src/main/java/solution/App.java
@@ -0,0 +1,13 @@
+package solution.src.main.java.solution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // How to define your own maven build?
+    }
+}
---
+++ src/boilerplates/python/solution.py
@@ -0,0 +1,19 @@
+from importlib.abc import InspectLoader
+import unittest
+
+# Problem description will go in as a comment below:
+"""
+Problem statement:
+
+<copied from email>
+"""
+
+# Your solution function here [CAN BE MODIFIED BY DEVELOPER]
+def solution(inputs):
+    pass
+
+class ProblemTester(unittest.TestCase):
+    pass # Test cases written by running AutomateTest json/fileapp
+
+if __name__ == "__main__":
+    unittest.main()
